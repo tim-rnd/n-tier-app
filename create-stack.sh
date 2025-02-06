778aaec3367e6f8fcfb3a5c05090ddc25e5abf6b
timyshark <timy.shark@outlook.com>
2025-02-05 19:54:43 -0800

    Apply profile for aws cli commands

--- create-stack.sh
+++ create-stack.sh
@@ -2,15 +2,17 @@
 # to configure per account
 # 1. run ls -sf stack-id-<account>.txt stack-id.txt
 # 2. edit create-stack.sh change parameterKey/Value to approperiate key name
+[ ! -z $1 ] && PROFILE=${1,,} || PROFILE="default"
 export counter=`cat stack-id.txt`
 counter=$((counter + 1))
 echo $counter  > stack-id.txt
 counter=$(printf "%06d" $counter)
 stack_id=N-TIER-APP-$counter
-echo "Creating stack $stack_id:"
-# KeyName:= demo_key | sandbx_key | devops_cloud9 | poc_key
+echo "Creating stack $stack_id: $PROFILE"
+# KeyName:= demo_key | sandbx_key | poc_key
 aws cloudformation create-stack --stack-name N-TIER-APP-$counter \
---template-body file:///home/aws/Projects/cloudFormation/n-tier-app/n-tier-app.json \
+--template-body file:///home/aws/projects/cloudFormation/n-tier-app/n-tier-app.json \
 --capabilities CAPABILITY_NAMED_IAM \
---parameters ParameterKey=KeyName,ParameterValue=demo_key
+--parameters ParameterKey=KeyName,ParameterValue=n-tier-key \
+--profile $PROFILE
 
